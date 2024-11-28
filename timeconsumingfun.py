6b0c20be00b12ea4cdc28a6dfa3bd594ed95426a
Pavan Kumar Etta <43481897+Elizah550@users.noreply.github.com>
2024-11-28 17:46:25 -0500

    Add files via upload

---
+++ timeconsumingfun.py
@@ -0,0 +1,11 @@
+import time
+
+def timeconsumingfun(arg):
+    time.sleep(5)
+
+# def timeconsumingfun(arg):
+#     # Generate a random integer between 1 and 5
+#     pause_duration = random.randint(1, 5) 
+#     print(f'Pausing for {pause_duration} seconds...')  # Display the pause duration
+#     time.sleep(pause_duration)  # Pause for the random duration
+
